02c34de1365b8b83872145c9b79387e4d7163f6a
Bruno de Carvalho <bruno@biasedbit.com>
2012-03-13 19:20:54 +0000

    Initial Commit

---
+++ BBCyclingLabelDemo/BBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BBAppDelegate.h
+//  BBCyclingLabelDemo
+//
+//  Created by Bruno de Carvalho on 3/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BBCyclingLabelDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BBCyclingLabelDemo
+//
+//  Created by Bruno de Carvalho on 3/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BBAppDelegate class]));
+    }
+}
