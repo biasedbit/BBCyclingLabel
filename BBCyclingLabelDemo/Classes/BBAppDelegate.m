c98897c49523ce9a01e5a7b8d3e41bfd1f8d87ec
Bruno de Carvalho <bruno@biasedbit.com>
2012-05-14 16:16:12 +0100

    Updated project license -- docs and backing post at biasedbit.com coming up

--- BBCyclingLabelDemo/Classes/BBAppDelegate.m
+++ BBCyclingLabelDemo/Classes/BBAppDelegate.m
@@ -1,18 +1,41 @@
 //
-//  BBAppDelegate.m
-//  BBCyclingLabelDemo
+// Copyright 2012 BiasedBit
 //
-//  Created by Bruno de Carvalho on 3/13/12.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+//
+//  Created by Bruno de Carvalho -- @biasedbit / http://biasedbit.com
 //  Copyright (c) 2012 BiasedBit. All rights reserved.
 //
 
 #import "BBAppDelegate.h"
 
+
+
+#pragma mark -
+
 @implementation BBAppDelegate
+
+
+#pragma mark Property synthesizers
 
 @synthesize window = _window;
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+
+#pragma mark UIApplicationDelegate
+
+- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
 {
     return YES;
 }
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Still a work in progress
 
 Here's a [demo video](http://d.pr/nMcl)
+
+
+## License
+
+This component is licensed under the Apache Software License 2.0
